6e3b2601fc8ec5563a6d37de07b2a28435fe2885
梁强 <liangqiang.lq@alibaba-inc.com>
2016-04-16 14:58:26 +0800

    列表使用Y000.plist文件

--- MyProject/MyProject/业务/iPhone课件Demo/Y000 目录/Y000ViewModel.m
+++ MyProject/MyProject/业务/iPhone课件Demo/Y000 目录/Y000ViewModel.m
@@ -20,14 +20,17 @@
 -(void)loadData{
     AJSectionItem *sectionItem = [self.sectionArray firstObject];
     [sectionItem.cellDataArray removeAllObjects];
-    
-    NSArray *array = @[@[@"Y001 UIButton", @"Y001ViewController"],
-                       @[@"Y002 UILabel", @"Y002ViewController"],
-                       @[@"Y003 UITextField", @"Y003ViewController"],
-                       ];
-    NSMutableArray *itemArray = [AJNormalItem createArray:array withType:@"PushVC"];
-    [sectionItem.cellDataArray addObjectsFromArray:itemArray];
-    
+        
+    NSString *bundleFile = [[NSBundle mainBundle] pathForResource: @"Y000" ofType: @"plist"];
+    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:bundleFile];
+    for (NSString *key in dict.allKeys) {
+        AJNormalItem *item = [AJNormalItem new];
+        item.title = key;
+        item.actionType = @"PushVC";
+        item.actionContent = [dict safeObjectFortKey:key];
+        [sectionItem.cellDataArray addObject:item];
+    }
+
     [self notifyToRefresh];
 }
 
