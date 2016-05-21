4d3be468eb8ced2fede90f00d0a20ec5332bd19b
TheYoungSmile <1069230922@qq.com>
2016-05-21 15:12:09 +0800

    多线程
    添加多线程页面进来

--- MyProject/MyProject/业务/iPhone课件Demo/Y022 多线程/Y022ViewController.m
+++ MyProject/MyProject/业务/iPhone课件Demo/Y022 多线程/Y022ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "Y022ViewController.h"
+#import "Y022ViewModel.h"
 
 @interface Y022ViewController ()
-
+@property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, strong) Y022ViewModel *viewModel;
 @end
 
 @implementation Y022ViewController
